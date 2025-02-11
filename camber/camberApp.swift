bba8e598ead2e11a4955c10fe8ed52558047d38b
plategpt <dev@plategpt.io>
2025-02-10 18:41:02 -0600

    Initial Commit

---
+++ camber/camberApp.swift
@@ -0,0 +1,17 @@
+//
+//  camberApp.swift
+//  camber
+//
+//  Created by Roddy Gonzalez on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct camberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
